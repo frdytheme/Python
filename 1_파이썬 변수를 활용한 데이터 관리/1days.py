af3213d6d7fe0daee67e18f4c7ab0685136435b7
FRDY <rbfridayy@gmail.com>
2024-08-22 18:46:48 +0900

    complete

--- 1_파이썬 변수를 활용한 데이터 관리/1days.py
+++ 1_파이썬 변수를 활용한 데이터 관리/1days.py
@@ -1,1 +1,4 @@
-print("Hello World!")
+print("Welcome to the Band Name Generator")
+hometown = input("당신의 고향은 어디입니까?")
+petname = input("당신의 애완동물 이름은?")
+print("당신의 밴드 이름은 " + "<" + hometown + " " + petname + ">")
